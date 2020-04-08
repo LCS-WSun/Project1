a02f7517cd8fbb2cc3dcf9d263a0f969fd0dc712
Wesley Sun <wsun@lcs.on.ca>
2020-04-08 13:44:19 -0400

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Sun, Wesley on 2020-04-08.
+//  Copyright © 2020 Wesley Sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
